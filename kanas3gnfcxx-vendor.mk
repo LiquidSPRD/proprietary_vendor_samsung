5d0f3a2be78b2d92b7306438fe62f9ae5f540ca5
Y300-0100 <bosko.brajovich@gmail.com>
2015-11-19 08:31:16 -0800

    z

---
+++ kanas3gnfcxx-vendor.mk
@@ -0,0 +1,31 @@
+	
+#
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_COPY_FILES += \
+vendor/samsung/kanas3gnfcxx/proprietary/lib:lib/libomx_m4vh263dec_sw_sprd.so \
+vendor/samsung/kanas3gnfcxx/proprietary/lib:lib/libomx_m4vh263dec_hw_sprd.so \
+vendor/samsung/kanas3gnfcxx/proprietary/lib:lib/libomx_m4vh263enc_hw_sprd.so \
+vendor/samsung/kanas3gnfcxx/proprietary/lib:lib/libomx_avcdec_hw_sprd.so \
+vendor/samsung/kanas3gnfcxx/proprietary/lib:lib/libomx_avcdec_sw_sprd.so \
+vendor/samsung/kanas3gnfcxx/proprietary/lib:lib/libomx_avcenc_hw_sprd.so \
+vendor/samsung/kanas3gnfcxx/proprietary/lib:lib/libomx_vpxdec_hw_sprd.so \
+vendor/samsung/kanas3gnfcxx/proprietary/lib:lib/libomx_aacdec_sprd.so \
+vendor/samsung/kanas3gnfcxx/proprietary/lib:lib/libomx_mp3dec_sprd.so \
+vendor/samsung/kanas3gnfcxx/proprietary/lib:lib/libril.so \
+vendor/samsung/kanas3gnfcxx/proprietary/lib:lib/libreference-ril_sp.so \
+vendor/samsung/kanas3gnfcxx/proprietary/bin:bin/rild \
+vendor/samsung/kanas3gnfcxx/proprietary/bin:bin/phoneserver
